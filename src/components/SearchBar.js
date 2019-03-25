172b8ab0881accb8bebe997094ff5ad19cefba27
Viktoras Domarkas <vidomarkas@gmail.com>
2019-03-25 22:05:04 +0000

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <form action="">
+                    <label htmlFor=""></label>
+                    <input type="text"/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
